a0caa8c8d2624ab48657e882319fe91383fc72c0
Augustonc <augustocasado93@gmail.com>
2023-06-01 16:02:45 -0300

    Se agregaron objetos de tipo computadora (11 en total) y se completo una lista del objeto orden1 de 11 elementos y solo se mostraron 10, el codigo funciona perfectamente

--- Leccion01/MundoPc/src/mundopc/mundoPC.java
+++ Leccion01/MundoPc/src/mundopc/mundoPC.java
@@ -19,11 +19,75 @@ public class mundoPC {
         Raton ratonGamer = new Raton("Bluetooth", "Gamer");
         Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
         
+        //Computadora Dell
+        Monitor monitorDell = new Monitor("Dell", 15);
+        Teclado tecladoDell = new Teclado("Bluetooth", "Dell");
+        Raton ratonDell = new Raton("Bluetooth", "Dell");
+        Computadora computadoraDell = new Computadora("Computadora Dell", monitorDell, tecladoDell, ratonDell);
+        
+        //Computadora Lenovo
+        Monitor monitorLenovo = new Monitor("Lenovo", 17);
+        Teclado tecladoLenovo = new Teclado("Bluetooth", "Lenovo");
+        Raton ratonLenovo = new Raton("Bluetooth", "Lenovo");
+        Computadora computadoraLenovo = new Computadora("Computadora Lenovo", monitorLenovo, tecladoLenovo, ratonLenovo);
+        
+        //Computadora Apple
+        Monitor monitorApple = new Monitor("Apple", 12);
+        Teclado tecladoApple = new Teclado("Bluetooth", "Apple");
+        Raton ratonApple = new Raton("Bluetooth", "Apple");
+        Computadora computadoraApple = new Computadora("Computadora Apple", monitorApple, tecladoApple, ratonApple);
+        
+        //Computadora Asus
+        Monitor monitorAsus = new Monitor("Asus", 22);
+        Teclado tecladoAsus = new Teclado("Bluetooth", "Asus");
+        Raton ratonAsus = new Raton("Bluetooth", "Asus");
+        Computadora computadoraAsus = new Computadora("Computadora Asus", monitorAsus, tecladoAsus, ratonAsus);
+        
+        //Computadora Acer
+        Monitor monitorAcer = new Monitor("Acer", 28);
+        Teclado tecladoAcer = new Teclado("Bluetooth", "Acer");
+        Raton ratonAcer = new Raton("Bluetooth", "Acer");
+        Computadora computadoraAcer = new Computadora("Computadora Acer", monitorAcer, tecladoAcer, ratonAcer);
+        
+        //Computadora Toshiba
+        Monitor monitorToshiba = new Monitor("Toshiba", 19);
+        Teclado tecladoToshiba = new Teclado("Bluetooth", "Toshiba");
+        Raton ratonToshiba = new Raton("Bluetooth", "Toshiba");
+        Computadora computadoraToshiba = new Computadora("Computadora Toshiba", monitorToshiba, tecladoToshiba, ratonToshiba);
+        
+        //Computadora MSI
+        Monitor monitorMSI = new Monitor("MSI", 14);
+        Teclado tecladoMSI = new Teclado("Bluetooth", "MSI");
+        Raton ratonMSI = new Raton("Bluetooth", "MSI");
+        Computadora computadoraMSI = new Computadora("Computadora MSI", monitorMSI, tecladoMSI, ratonMSI);
+
+        //Computadora Sony
+        Monitor monitorSony = new Monitor("Sony", 23);
+        Teclado tecladoSony = new Teclado("Bluetooth", "Sony");
+        Raton ratonSony = new Raton("Bluetooth", "Sony");
+        Computadora computadoraSony = new Computadora("Computadora Sony", monitorSony, tecladoSony, ratonSony);
+
+        //Computadora Samsung (Esta es la computadora N° 11)
+        Monitor monitorSamsung = new Monitor("Samsung", 22);
+        Teclado tecladoSamsung = new Teclado("Bluetooth", "Samsung");
+        Raton ratonSamsung = new Raton("Bluetooth", "Samsung");
+        Computadora computadoraSamsung = new Computadora("Computadora Samsung", monitorSamsung, tecladoSamsung, ratonSamsung);
+
+        
         Orden orden1 = new Orden();//Inicializamos el arreglo
         Orden orden2 = new Orden();//Una nueva lista para el objeto orden2
         
         orden1.agregarComputadora(computadoraHP);
-        orden1.agregarComputadora(computadoraGamer);
+        orden1.agregarComputadora(computadoraGamer);        
+        orden1.agregarComputadora(computadoraDell);
+        orden1.agregarComputadora(computadoraLenovo);
+        orden1.agregarComputadora(computadoraApple);
+        orden1.agregarComputadora(computadoraAsus);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraToshiba);
+        orden1.agregarComputadora(computadoraMSI);
+        orden1.agregarComputadora(computadoraSony);
+        orden1.agregarComputadora(computadoraSamsung);
         
         Computadora ComputadorasVarias = new Computadora("Computadora de diferentes marcas: ", monitorHP, tecladoGamer, ratonHP);
         orden2.agregarComputadora(ComputadorasVarias);
